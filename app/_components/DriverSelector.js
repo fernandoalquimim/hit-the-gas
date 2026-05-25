f618c607d487adbab328025f1dfe06f8302e7bcd
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-05-25 10:57:31 -0300

    Improving driver selector component

--- app/_components/DriverSelector.js
+++ app/_components/DriverSelector.js
@@ -10,20 +10,28 @@ function DriverSelector() {
   const { hasDriver, setHasDriver } = useReservation();
 
   return (
-    <div className="flex justify-between items-center shadow w-full border border-primary-800 has-checked:ring-[#5fa212] has-checked:ring-2 bg-primary-950 px-6 py-5 rounded-md mb-10">
-      <div class="flex items-center space-x-5">
-        <div class="flex items-center relative w-16 h-16">
-          <Image src={pilot} fill className="object-cover" alt="pilot" />
+    <label className="driver-checkbox-container">
+      <div className="flex justify-around items-center space-x-5 shadow w-full border border-primary-800 has-checked:ring-[#5fa212] has-checked:ring-2 bg-primary-950 px-6 rounded-md mb-10">
+        <div class="flex items-center space-x-5">
+          <div class="flex items-center relative w-32 h-32">
+            <Image src={pilot} fill className="object-cover" alt="pilot" />
+          </div>
+          <h2 class="text-3xl">I want an expert driver to take the wheel</h2>
         </div>
-        <h2 class="text-3xl">I want an expert driver to take to wheel</h2>
+        <input
+          type="checkbox"
+          checked={hasDriver}
+          onChange={() => setHasDriver((h) => !h)}
+        />
+        <svg viewBox="0 0 64 64" className="h-8 w-8">
+          <path
+            d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+            pathLength="575.0541381835938"
+            className="driver-checkbox-path"
+          ></path>
+        </svg>
       </div>
-      <input
-        type="checkbox"
-        class="h-9 w-9 outline-offset-2 origin-left accent-[#5fa212]"
-        checked={hasDriver}
-        onChange={() => setHasDriver((h) => !h)}
-      />
-    </div>
+    </label>
   );
 }
 
