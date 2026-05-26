9a626d67c45a0810b1c8d47d734f8a6b773851f5
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-05-26 00:06:48 -0300

    Adding loading indicator on reservation form's submit button

--- app/_components/SubmitButton.js
+++ app/_components/SubmitButton.js
@@ -1,7 +1,18 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+import SpinnerMini from "./SpinnerMini";
+
 function SubmitButton({ children }) {
+  const { pending } = useFormStatus();
+
   return (
-    <button className="flex justify-center bg-accent-500 w-52 h-15 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-      {children}
+    <button
+      className="flex justify-center bg-accent-500 w-52 h-15 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? <SpinnerMini className="mx-auto" /> : children}
     </button>
   );
 }
