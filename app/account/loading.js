bb5d46203f26898850d55e6501c5f8d1a796526b
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-05-29 14:28:53 -0300

    Adjusting account loading indicator

--- app/account/loading.js
+++ app/account/loading.js
@@ -1,5 +1,9 @@
 import Spinner from "@/app/_components/Spinner";
 
 export default function Loading() {
-  return <Spinner />;
+  return (
+    <div className="flex flex-col justify-start py-50 h-full">
+      <Spinner />
+    </div>
+  );
 }
