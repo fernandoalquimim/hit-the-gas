6ee6b81a250f1a1aef89d78d135981cb7b5daf50
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-06-02 13:50:48 -0300

    Adding callback url in car detail page's login message

--- app/_components/LoginMessage.js
+++ app/_components/LoginMessage.js
@@ -1,11 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function LoginMessage() {
+  const pathname = usePathname();
+
   return (
     <div className="grid bg-primary-800 ">
       <p className="text-center text-xl py-12 self-center">
         Please{" "}
-        <Link href="/login" className="underline text-accent-500">
+        <Link
+          href={`/login?callbackUrl=${encodeURIComponent(pathname)}`}
+          className="underline text-accent-500"
+        >
           login
         </Link>{" "}
         to reserve this
