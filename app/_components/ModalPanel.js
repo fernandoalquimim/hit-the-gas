8cdfbf565df6b3106d595f045567fc72991d85e5
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-06-08 23:23:10 -0300

    Adding fade effect on modal window

--- app/_components/ModalPanel.js
+++ app/_components/ModalPanel.js
@@ -1,6 +1,6 @@
 function ModalPanel({ children }) {
   return (
-    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-[0_0_3.2rem_1rem_rgba(0,0,0,1)] px-14 py-16 transition-all duration-500 bg-primary-900">
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-[0_0_3.2rem_1rem_rgba(0,0,0,1)] px-14 py-16 transition-all duration-500 bg-primary-900 fade-div">
       {children}
     </div>
   );
