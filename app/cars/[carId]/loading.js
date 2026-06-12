92fda9b703369976b5005b91cb3da552c80d7510
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-06-12 00:06:20 -0300

    Adding loading indicator to car details page

---
+++ app/cars/[carId]/loading.js
@@ -0,0 +1,13 @@
+import Spinner from "@/app/_components/Spinner";
+import Main from "@/app/_components/Main";
+
+export default function Loading() {
+  return (
+    <Main>
+      <div className="grid justify-center items-center">
+        <Spinner />
+        <p>Loading car...</p>
+      </div>
+    </Main>
+  );
+}
