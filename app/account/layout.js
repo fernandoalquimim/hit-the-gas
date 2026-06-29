cfcb1857fc0a0f2771f876b5c0fad2119d7bdbf6
Fernando Alquimim <fernando.freire.alquimim@gmail.com>
2026-06-29 17:50:43 -0300

    Adding responsiveness to account section layout

--- app/account/layout.js
+++ app/account/layout.js
@@ -4,7 +4,7 @@ import SideNavigation from "@/app/_components/SideNavigation";
 export default function Layout({ children }) {
   return (
     <Main>
-      <div className="grid grid-cols-[16rem_1fr] flex-col flex-1 w-full min-h-0 h-[calc(100vh-11rem)] overflow-hidden gap-10">
+      <div className="grid grid-cols-[4rem_1fr] lg:grid-cols-[16rem_1fr] flex-col flex-1 w-full min-h-0 h-[calc(100vh-11rem)] overflow-hidden gap-10 transition-all duration-300">
         <div className="h-full overflow-hidden">
           <SideNavigation />
         </div>
